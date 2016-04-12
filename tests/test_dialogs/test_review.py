1be47b81053a1662b7e90dd5122a0654e250ba5c
Colin Duquesnoy <colin.duquesnoy@gmail.com>
2016-04-12 18:23:55 +0200

    Fix test: window icon need to be specified now

--- tests/test_dialogs/test_review.py
+++ tests/test_dialogs/test_review.py
@@ -2,7 +2,7 @@ from qcrash._dialogs.review import DlgReview
 
 
 def test_review(qtbot):
-    dlg = DlgReview('some content', 'log content', None)
+    dlg = DlgReview('some content', 'log content', None, None)
     assert dlg.ui.edit_main.toPlainText() == 'some content'
     assert dlg.ui.edit_log.toPlainText() == 'log content'
     qtbot.keyPress(dlg.ui.edit_main, 'A')
