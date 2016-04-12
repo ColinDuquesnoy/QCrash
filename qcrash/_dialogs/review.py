86e1ad242f73f32d1c782d46d24ceb5eca21b7bd
ColinDuquesnoy <colin.duquesnoy@gmail.com>
2016-04-12 18:14:58 +0200

    Fix GUI issues in dialogs
    - fix missing dialog icon
    - remove help button from dialogs
    
    Close OpenCobolIDE/OpenCobolIDE#303

--- qcrash/_dialogs/review.py
+++ qcrash/_dialogs/review.py
@@ -1,7 +1,7 @@
 """
 This module contains the review dialog.
 """
-from qcrash.qt import QtWidgets
+from qcrash.qt import QtCore, QtGui, QtWidgets
 from qcrash._forms import dlg_review_ui
 
 
@@ -9,7 +9,7 @@ class DlgReview(QtWidgets.QDialog):
     """
     Dialog for reviewing the final report.
     """
-    def __init__(self, content, log, parent):
+    def __init__(self, content, log, parent, window_icon):
         """
         :param content: content of the final report, before review
         :param parent: parent widget
@@ -18,13 +18,17 @@ class DlgReview(QtWidgets.QDialog):
         self.ui = dlg_review_ui.Ui_Dialog()
         self.ui.setupUi(self)
         self.ui.edit_main.setPlainText(content)
+        self.setWindowFlags(self.windowFlags() & ~QtCore.Qt.WindowContextHelpButtonHint)
+        self.setWindowIcon(
+            QtGui.QIcon.fromTheme('document-edit')
+            if window_icon is None else window_icon)
         if log:
             self.ui.edit_log.setPlainText(log)
         else:
             self.ui.tabWidget.tabBar().hide()
 
     @classmethod
-    def review(cls, content, log, parent):  # pragma: no cover
+    def review(cls, content, log, parent, window_icon):  # pragma: no cover
         """
         Reviews the final bug report.
 
@@ -34,7 +38,9 @@ class DlgReview(QtWidgets.QDialog):
         :returns: the reviewed report content or None if the review was
                   canceled.
         """
-        dlg = DlgReview(content, log, parent)
+        print('review icon', window_icon)
+        dlg = DlgReview(content, log, parent, window_icon)
         if dlg.exec_():
-            return dlg.ui.edit_main.toPlainText(), dlg.ui.edit_log.toPlainText()
+            return dlg.ui.edit_main.toPlainText(), \
+                dlg.ui.edit_log.toPlainText()
         return None, None
